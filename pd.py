c74d9ffe086ad6ed4ab5c36282bbe9215006abef
GUILHERME BRASIL PEREIRA <guibrasilpereira@outlook.com>
2025-10-20 15:18:21 -0300

    UC6

---
+++ pd.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+df = pd.read_csv("vendas_completas.csv", parse_dates=['Data_Venda'])
+
+print(f"Primeiras 5 linhas de DataFrame de vendas:\n{df.head()}")
+
+print(f"\nÚltimas 5 linhas de DataFrame de vendas:\n{df.tail()}")
+
+print(f"\nInformações sobre o DataFrame: {df.info()}")
+
+print(f"\nO DataFrame de vendas têm:\n{df.shape[0]} linhas e {df.shape[1]} colunas.")
+
+print("\nEstatística descritiva do DataFrame:")
+print(df.describe())
+
+
+print("\nInformações do índice:")
+print(df.index)
